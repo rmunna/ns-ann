35efbaac6008b692723236d7a11592e31354079e
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 23:26:54 +0530

    Update RssItem.java

--- RssItem.java
+++ RssItem.java
@@ -1,24 +1,46 @@
-
 public class RssItem {
     private String title;
     private String link;
     private String pubDate;
+    private String description;
 
-    public RssItem(String title, String link, String pubDate) {
+    public RssItem(String title, String link, String pubDate, String description) {
         this.title = title;
         this.link = link;
         this.pubDate = pubDate;
+        this.description = description;
     }
 
+    // Getters and setters
     public String getTitle() {
         return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getLink() {
         return link;
     }
 
+    public void setLink(String link) {
+        this.link = link;
+    }
+
     public String getPubDate() {
         return pubDate;
+    }
+
+    public void setPubDate(String pubDate) {
+        this.pubDate = pubDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
