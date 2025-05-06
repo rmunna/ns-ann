f23f97bb11764eea9bc8f479c7e85c005e3bbc91
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 23:29:34 +0530

    Create OnlineAnnouncementsFetcherService.java

---
+++ OnlineAnnouncementsFetcherService.java
@@ -0,0 +1,44 @@
+package com.example.nsann.service;
+
+import org.springframework.stereotype.Service;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OnlineAnnouncementsFetcherService {
+    // RSS feed URL for Online Announcements
+    private final String RSS_FEED_URL = "https://nsearchives.nseindia.com/content/RSS/Online_announcements.xml";
+
+    public List<RssItem> fetchOnlineAnnouncements() {
+        List<RssItem> rssItems = new ArrayList<>();
+        try {
+            URL url = new URL(RSS_FEED_URL);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            InputStream inputStream = connection.getInputStream();
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document doc = builder.parse(inputStream);
+
+            NodeList itemList = doc.getElementsByTagName("item");
+            for (int i = 0; i < itemList.getLength(); i++) {
+                Element item = (Element) itemList.item(i);
+                String title = item.getElementsByTagName("title").item(0).getTextContent();
+                String link = item.getElementsByTagName("link").item(0).getTextContent();
+                String pubDate = item.getElementsByTagName("pubDate").item(0).getTextContent();
+                String description = item.getElementsByTagName("description").item(0).getTextContent();
+
+                rssItems.add(new RssItem(title, link, pubDate, description));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return rssItems;
+    }
+}
