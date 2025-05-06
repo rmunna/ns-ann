f04c5ef4f4970d61baeac00e1883b1f007f7c728
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 22:01:11 +0530

    Create WhatsAppService.java

---
+++ WhatsAppService.java
@@ -0,0 +1,10 @@
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WhatsAppService {
+    public void sendMessage(String message) {
+        // Replace this with logic to send WhatsApp messages via an API
+        System.out.println("Sending message: " + message);
+    }
+}
