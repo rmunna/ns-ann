a891347bedc4db1ab92751566e3606d69689dceb
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 21:59:09 +0530

    Create NsAnnApplication.java

---
+++ com/com/example/nsann/NsAnnApplication.java
@@ -0,0 +1,11 @@
+package com.example.nsann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NsAnnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NsAnnApplication.class, args);
+    }
+}
