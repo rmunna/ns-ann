c402c1455513d9c86a01ff39c8443a82a69bc6ef
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 23:31:26 +0530

    Update RssFeedRunner.java

--- RssFeedRunner.java
+++ RssFeedRunner.java
@@ -4,15 +4,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class RssFeedRunner implements CommandLineRunner {
     private final SchedulerService schedulerService;
+    private final OnlineAnnouncementsFetcherService onlineAnnouncementsFetcherService;
 
-    public RssFeedRunner(SchedulerService schedulerService) {
+    public RssFeedRunner(SchedulerService schedulerService, OnlineAnnouncementsFetcherService onlineAnnouncementsFetcherService) {
         this.schedulerService = schedulerService;
+        this.onlineAnnouncementsFetcherService = onlineAnnouncementsFetcherService;
     }
 
     @Override
     public void run(String... args) {
-        if (args.length > 0 && args[0].equals("processRssFeed")) {
-            schedulerService.processRssFeed();
+        if (args.length > 0) {
+            if (args[0].equals("processRssFeed")) {
+                schedulerService.processRssFeed();
+            } else if (args[0].equals("fetchOnlineAnnouncements")) {
+                onlineAnnouncementsFetcherService.fetchOnlineAnnouncements();
+            }
         }
     }
 }
