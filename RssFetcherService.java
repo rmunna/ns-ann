bae1213bc4f791bb20c4875960e7f6113763cfcc
Rajakumar Munna <nameismunna@gmail.com>
2025-05-06 23:28:16 +0530

    Update RssFetcherService.java

--- RssFetcherService.java
+++ RssFetcherService.java
@@ -30,6 +30,7 @@ public class RssFetcherService {
                 Element item = (Element) itemList.item(i);
                 String title = item.getElementsByTagName("title").item(0).getTextContent();
                 String link = item.getElementsByTagName("link").item(0).getTextContent();
+                String pubDate = item.getElementsByTagName("pubDate").item(0).getTextContent();
                 String description = item.getElementsByTagName("description").item(0).getTextContent();
 
                 if (title.contains("Financial Result")) {
